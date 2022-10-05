3f99f04d54b165fb7529cf4ac2a89117f3b14eb4
MaTu <ivancostilla17@gmail.com>
2022-10-05 09:01:16 -0300

    post test

--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,9 @@ router.get("/test", (req,res) =>{
     res.send("user test ok")
 })
 
+router.post("/testpost", (req,res) => {
+    const username = req.body.username
+    res.send(`Your username is ${username}`)
+})
+
 module.exports = router
