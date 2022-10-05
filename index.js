02221d1456f0d500c9d2760ad148f7eb33f60626
MaTu <ivancostilla17@gmail.com>
2022-10-05 15:39:16 -0300

    crypto hash

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const mongoose = require('mongoose')
 const routerSh = require("./routes/user")
+const auth = require("./routes/auth")
 require('dotenv').config()
 
 mongoose.connect(process.env.MONGODB).then(() => console.log("DB connection on")).catch((err) => {
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGODB).then(() => console.log("DB connection on")
 app.use(express.json())
 
 app.use('/api',routerSh)
+app.use('/auth',auth )
 
 app.listen(process.env.PORT || 8090, () => {
     console.log("Server on in PORT - ", process.env.PORT )
