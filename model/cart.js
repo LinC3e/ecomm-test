2763711243fae9e2148f157d974875497a20d294
MaTu <ivancostilla17@gmail.com>
2022-10-05 14:14:27 -0300

    Cart schema

---
+++ model/cart.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+    userID: { type: String, required: true },
+    products : [
+        {
+            productID: { type : String },
+            quantity: { type: Number , default: 1}
+        }
+    ]
+})
+
+module.exports = mongoose.model('usuarios',productSchema)
