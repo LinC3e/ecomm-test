9033ca3b45491450fd6fe37e18d9b4b883a5cc61
MaTu <ivancostilla17@gmail.com>
2022-10-05 08:41:57 -0300

    mongo connection

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+mongoose.connect(process.env.MONGODB).then(() => console.log("DB connection on")).catch((err) => {
+    console.log(err)
+})
+
+
 app.listen(process.env.PORT || 8090, () => {
     console.log("Server on in PORT - ", process.env.PORT )
 })
