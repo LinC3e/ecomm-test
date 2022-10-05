f1771d17cd41301c13c57d701e297a8d308a8dbf
MaTu <ivancostilla17@gmail.com>
2022-10-05 08:33:33 -0300

    app start

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("test")
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+app.listen(process.env.PORT || 8090, () => {
+    console.log("Server on in PORT - ", process.env.PORT )
+})
